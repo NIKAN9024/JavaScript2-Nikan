e4e77e4ab08ea958aed9eb6c7628e37b340053b6
NIKAN9024 <9024387@student.tcrmbo.nl>
2026-09-23 12:47:46 +0200

    ....

--- week-03-moderne-syntax/oefening-2/script.js
+++ week-03-moderne-syntax/oefening-2/script.js
@@ -1,8 +1,8 @@
 // Voeg een event listener toe aan elke knop
 // Knop 1: voeg tekst toe aan #message
-btn1.addEventListener('click', function () {
-    message.textContent = 'Je hebt op de knop geklikt!';
-})
+btn1.addEventListener("click", function () {
+  message.textContent = "Je hebt op de knop geklikt!";
+});
 
 btn2.addEventListener("click", function () {
   message.textContent = "Je hebt op de tweede knop geklikt!";
@@ -12,6 +12,8 @@ btn3.addEventListener("click", function () {
   message.textContent = "Je hebt op de derde knop geklikt!";
 });
 
-
 // Knop 2: voeg een <li> toe aan #list met een tekst
 // Knop 3: wissel de klasse 'active' op #message
+document.getElementById("active");
+
+active.textContent = "message";
