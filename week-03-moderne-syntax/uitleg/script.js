96c0972d5629f95f8ee12881af1df0623d7ada0c
NIKAN9024 <9024387@student.tcrmbo.nl>
2026-09-23 13:21:13 +0200

    oefening 2 gemaakt

--- week-03-moderne-syntax/uitleg/script.js
+++ week-03-moderne-syntax/uitleg/script.js
@@ -1,24 +1,39 @@
-let getName = document.getElementById('showName')
-
+let getName = document.getElementById("showName");
 
 // function showName(name) {
-//     return "Mijn naam is: " + name 
+//     return "Mijn naam is: " + name
 // }
 
 const showName = (name) => {
-    return `Mijn naam is:  ${name} `
-}
+  return `Mijn naam is:  ${name} `;
+};
 
-getName.textContent = showName("Nikan")
+getName.textContent = showName("Nikan");
 
-let fruits = ['Appel', 'Banaan', 'Perzik'] 
+let fruits = ["Appel", "Banaan", "Perzik"];
 
 // for (let i = 0; i < fruits.length; i ++) {
 //     console.log(fruits[1])
 // }
 
 for (let fruit of fruits) {
-    getName.innerHTML += fruit + "<br>";
+  getName.innerHTML += fruit + "<br>";
 }
 
+let title = document.getElementById("title");
+let button = document.getElementById("button");
+let section = document.getElementById("section");
 
+button.addEventListener("click", () => {
+  title.textContent = "Ik heb geklikt!";
+
+    title.classList.toggle("active");
+    
+
+    const p = document.createElement("p");
+
+    p.textContent = "Ik voeg een paragraaf toe";
+
+
+    section.appendChild(p);
+});
