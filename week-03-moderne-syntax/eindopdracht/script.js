e051dfebef5114709f105fc448048e62c879b0a4
NIKAN9024 <9024387@student.tcrmbo.nl>
2026-09-25 12:55:42 +0200

    commit

--- week-03-moderne-syntax/eindopdracht/script.js
+++ week-03-moderne-syntax/eindopdracht/script.js
@@ -1,3 +1,5 @@
 // Stap 1: Selecteer het formulier en de profielenlijst
+let form = document.getElementById("profile-form");
+let formList = document.getElementById("profiles-list");
 // Stap 2: Luister naar het submit-event, lees de invoervelden uit met .value en toon een profielkaart met innerHTML +=
 // Stap 3 (bonus): Voeg een verwijderknop toe aan elke kaart
